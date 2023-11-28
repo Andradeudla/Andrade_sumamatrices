6f013d9b75b517861fc921fdd7f089ffde5a3ce4
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-11-28 14:54:45 -0500

    segundo commit

--- suma.c
+++ suma.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
+#include <stdio.h> //se incluyen las bibliotecas para las variables y los numeros random
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int main() // la variable principal del programa de inicializacion
 {
+    printf("----------Bienvenido al Programa de Suma de Matrices---------- \n"); // se agrega un titulo para dar mejor presentacion
+
+    int fila, columna;                                // se define las variables para filas y columnas
+    printf("ingrese el valor para filas: "); // se solicita al usuario ingresar el numero de filas
+    scanf("%d", &fila);                         // se lee los datos ingresados por el usuario
+
+    printf("ingrese el valor para columnas: "); // se solicita al usuario ingresar el numero de columnas
+    scanf("%d", &columna);                            // se lee los datos ingresados por el usuario
+
+    int matrix[fila][columna];  // se define las variables con el numero de espacios ingresados por el usuario de filas y columnas.
+    srand(time(NULL)); // se crea la funcion para numeros randoms
+
+    printf("---Esta es la Matriz A--- \n");
+
+    for (int i = 0; i < fila; i++) // se crea el primerfor para la matriz original de las filas
+    {
+        printf("\n");               // se coloca para separa las filas
+        for (int j = 0; j < columna; j++) // Se crea en segundo for para la matriz original de columnas
+        {
+            matrix[i][j] = rand() % 100 + 1; // Se define que la matriz se ponga el rango de numeros random que se solicitan en cada cuadro de la matriz
+            printf("%d \t", matrix[i][j]);   // se inprimer la matriz con numeros random
+        }
+    }
 
     return 0;
 }
